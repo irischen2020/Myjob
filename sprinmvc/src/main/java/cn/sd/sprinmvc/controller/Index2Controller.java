53181c4a8cdf4b8e79358da352086dd33a28e155
leiyufang <leiyufang@sunlands.com>
2020-03-27 16:40:16 +0800

    sprinmvc

--- sprinmvc/src/main/java/cn/sd/sprinmvc/controller/Index2Controller.java
+++ sprinmvc/src/main/java/cn/sd/sprinmvc/controller/Index2Controller.java
@@ -5,9 +5,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Index2Controller {
-	
-	@RequestMapping("/testredirect")
-	public String testRedirect(){
-		return "redirect:/test";
-	}
+
 }
