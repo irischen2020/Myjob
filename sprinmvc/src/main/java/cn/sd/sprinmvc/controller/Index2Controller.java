79a5ff3f67b3d3c89fbf01c77cf4d5c1a4a1d32d
leiyufang <leiyufang@sunlands.com>
2020-03-27 17:55:43 +0800

    sprinmvc

--- sprinmvc/src/main/java/cn/sd/sprinmvc/controller/Index2Controller.java
+++ sprinmvc/src/main/java/cn/sd/sprinmvc/controller/Index2Controller.java
@@ -2,8 +2,29 @@ package cn.sd.sprinmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Index2Controller {
+	
+	@RequestMapping(value="/testGet",method = RequestMethod.GET)
+	public String testGet(){
+		return "success";
+	}
+	
+	@RequestMapping(value="/testPost",method = RequestMethod.POST)
+	public String testPost(){
+		return "success";
+	}
+	
+	@RequestMapping(value="/testPut",method = RequestMethod.PUT)
+	public String testPut(){
+		return "success";
+	}
+	
+	@RequestMapping(value="/testGet",method = RequestMethod.DELETE)
+	public String testDelete(){
+		return "success";
+	}
 
 }
