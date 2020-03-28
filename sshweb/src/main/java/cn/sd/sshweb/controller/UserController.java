ba7c5976d478399937275527c3337f3efdc3741d
iriscj2020 <309034900@qq.com>
2020-03-28 23:27:05 +0800

    sshweb

--- sshweb/src/main/java/cn/sd/sshweb/controller/UserController.java
+++ sshweb/src/main/java/cn/sd/sshweb/controller/UserController.java
@@ -1,4 +1,10 @@
 package cn.sd.sshweb.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+    public UserController(){
+        System.out.println("UserController constructor runs");
+    }
 }
