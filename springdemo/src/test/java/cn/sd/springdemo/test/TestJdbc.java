00be2c88d6b8b9b954791070d94bb55d8d90bc3b
iriscj2020 <309034900@qq.com>
2020-03-29 23:33:49 +0800

    SPRINGDEMO

---
+++ springdemo/src/test/java/cn/sd/springdemo/test/TestJdbc.java
@@ -0,0 +1,22 @@
+package cn.sd.springdemo.test;
+
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+
+public class TestJdbc {
+
+    @Test
+    public void test() throws SQLException{
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+        System.out.println(dataSource.getConnection());
+    }
+}
