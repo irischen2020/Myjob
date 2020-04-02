18babc5ac53261765dc558511ce280f2353d3a86
leiyufang <leiyufang@sunlands.com>
2020-04-02 18:53:28 +0800

    swagger2

--- testdev/src/main/java/com/sd/controller/UserController.java
+++ testdev/src/main/java/com/sd/controller/UserController.java
@@ -3,6 +3,8 @@ package com.sd.controller;
 
 import com.sd.common.Result;
 import com.sd.pojo.User;
+import com.sd.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    
+    @Autowired
+    private UserService userService;
     //注册方法
     @RequestMapping("/register")
     public Result register(User user) {
-
-        return null;
+        //调用业务层方法，插入到DB，后面统一处理异常
+        boolean s = userService.save(user);
+        Result result = new Result("1","注册成功");
+        return result;
     }
 
     //账号验重
