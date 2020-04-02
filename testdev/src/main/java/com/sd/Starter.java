6cad6ee63391657ad2d33b6ff5dd481b3dd81a7f
leiyufang <leiyufang@sunlands.com>
2020-04-02 19:22:42 +0800

    swagger2

--- testdev/src/main/java/com/sd/Starter.java
+++ testdev/src/main/java/com/sd/Starter.java
@@ -3,11 +3,13 @@ package com.sd;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @MapperScan(basePackages = "com.sd.mapper")
+@EnableTransactionManagement
 public class Starter {
 	public static void main(String[] args){
 		SpringApplication.run(Starter.class,args);
--- testdev/src/main/java/com/sd/controller/UserController.java
+++ testdev/src/main/java/com/sd/controller/UserController.java
@@ -5,6 +5,7 @@ import com.sd.common.Result;
 import com.sd.pojo.User;
 import com.sd.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,8 @@ public class UserController {
     @Autowired
     private UserService userService;
     //注册方法
-    @RequestMapping("/register")
+//    @RequestMapping("/register")
+    @PostMapping("/register")
     public Result register(User user) {
         //调用业务层方法，插入到DB，后面统一处理异常
         boolean s = userService.save(user);
