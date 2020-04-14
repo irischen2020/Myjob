11ed0169aa5010b2eecc81f4b09b3075391ecbbb
leiyufang <leiyufang@sunlands.com>
2020-04-14 16:16:19 +0800

    MyRealm

--- testdev/src/main/java/com/sd/controller/ProjectController.java
+++ testdev/src/main/java/com/sd/controller/ProjectController.java
@@ -1,9 +1,18 @@
 package com.sd.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.sd.common.Result;
+import com.sd.service.ProjectService;
+import com.sd.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/project")
 public class ProjectController {
+	
+	@Autowired
+	private ProjectService projectService;
+	
+	@GetMapping("/toList")
+	@ApiOperation(value="获取项目列表方法",httpMethod = "GET")
+	public Result toList(Integer userId){
+		Result result = null;
+		QueryWrapper queryWrapper = new QueryWrapper();
+		queryWrapper.eq("create_user",userId);
+		List list = projectService.list(queryWrapper);
+		result = new Result("1",list,"项目列表");
+		return result;
+	}
 
 }
