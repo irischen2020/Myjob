72af41d93a5e10b08483f0b96043b3f75f71444e
leiyufang <leiyufang@sunlands.com>
2020-04-15 17:04:43 +0800

    mybatisdemo

--- mybatisdemo/src/main/java/cn/sd/mybatisdemo/model/User.java
+++ mybatisdemo/src/main/java/cn/sd/mybatisdemo/model/User.java
@@ -1,16 +1,25 @@
 package cn.sd.mybatisdemo.model;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class User {
 	private int id;
 	private String username;
-	private String pasword;
-	private Date regDate;
+	private String password;
+	private Date regtime;
 	
 	
 	public User() {
 		super();
+	}
+	
+	public User(String username, String password, Date regtime) {
+		this.username = username;
+		this.password = password;
+		this.regtime = regtime;
 	}
 }
 
--- mybatisdemo/src/test/java/cn/sd/mybatisdemo/Test/MybatisTest.java
+++ mybatisdemo/src/test/java/cn/sd/mybatisdemo/Test/MybatisTest.java
@@ -1,7 +1,5 @@
 package cn.sd.mybatisdemo.Test;
 
-
-import cn.sd.mybatisdemo.mapper.UserMapper;
 import cn.sd.mybatisdemo.model.User;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -18,13 +16,14 @@ public class MybatisTest {
 	public void test() throws IOException {
 		//1:获取到创建数据库链接的会话的工厂类,根据配置文件创建
 		InputStream inputStream = getResourceAsStream("mybatisConfig.xml");
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder()
+				.build(inputStream);
 		//2:通过工厂类，获取到数据库链接的会话
 		SqlSession session = sqlSessionFactory.openSession();
 		//3:通过session操作数据库
 		try {
-			UserMapper mapper = session.getMapper(UserMapper.class);
-			User user = mapper.getOneUserById(1);
+			User user = session.selectOne("cn.sd.mybatisdemo.mapper.getOneUserById",1);
+			System.out.println(user);
 		}finally {
 			session.close();
 		}
