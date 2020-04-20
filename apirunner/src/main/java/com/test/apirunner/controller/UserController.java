42b24be8627d22c9267e1a3219a1a55e3118d7e0
leiyufang <leiyufang@sunlands.com>
2020-04-20 14:47:19 +0800

    UserController

---
+++ apirunner/src/main/java/com/test/apirunner/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.test.apirunner.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.WebParam;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+	@RequestMapping("/toRegister")
+	public ModelAndView toRegister(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("register");
+		return modelAndView;
+	}
+}
