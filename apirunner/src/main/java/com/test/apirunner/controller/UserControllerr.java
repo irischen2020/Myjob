bd0c644d3bf9906b6bc0eab713f41ad9ce14557d
leiyufang <leiyufang@sunlands.com>
2020-04-21 17:42:01 +0800

    UserController

---
+++ apirunner/src/main/java/com/test/apirunner/controller/UserControllerr.java
@@ -0,0 +1,28 @@
+package com.test.apirunner.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserControllerr {
+	
+	@RequestMapping(value = "/toRegister")
+	public ModelAndView toRegister(){
+		ModelAndView modelAndView = new ModelAndView();
+		//准备数据
+		List<String> names = new ArrayList<>();
+		names.add("11");
+		names.add("22");
+		//设置要传到模板中的数据
+		modelAndView.addObject("names",names);
+		modelAndView.setViewName("showNames");
+//		modelAndView.setViewName("register");
+		return modelAndView;
+	}
+}
