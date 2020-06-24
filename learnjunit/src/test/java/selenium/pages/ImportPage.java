90cf28609334c360a0091c94ab62fcbf8256210d
leiyufang <leiyufang@sunlands.com>
2020-06-24 16:49:03 +0800

    PO-importfile

--- learnjunit/src/test/java/selenium/pages/ImportPage.java
+++ learnjunit/src/test/java/selenium/pages/ImportPage.java
@@ -5,13 +5,10 @@ import org.openqa.selenium.By;
 public class ImportPage extends BasePage {
 	public ContactPage importFormFile(String filepath)
 	{
-//		findElement(By.cssSelector(".js_upload_label")).sendKeys(filepath);
+		findElement(By.id("js_upload_file_input"),0).sendKeys(filepath);
 		
-//		findElement(By.cssSelector(".js_upload_label")).click();
-		findElement(By.id("js_upload_file_input")).sendKeys(filepath);
-		
-//		findElement(By.linkText("确认导入"));
-//		findElement(By.linkText("前往查看"));
+		findElement(By.linkText("确认导入")).click();
+		findElement(By.linkText("前往查看")).click();
 		return new ContactPage();
 	}
 	
