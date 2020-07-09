9ef143044f9ed10a94d4bdfce9d22120232a3eaf
leiyufang <leiyufang@sunlands.com>
2020-07-09 18:46:28 +0800

    PO-Message

---
+++ learnjunit/src/test/java/service/TestWork.java
@@ -0,0 +1,26 @@
+package service;
+
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestWork {
+	public static String token;
+	@BeforeAll
+	public void getToken() {
+		token =
+		given()
+				.param("corpid", "wwc135398b05beab7c")
+				.param("corpsecret", "cPMWkjGTqDxhI3dWGjFgGnQE03KH40F3yPtm8iSTyvc")
+		.when()
+				.log().all()
+				.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken")
+		.then()
+				.log().all()
+				.body("errcode",equalTo(0))
+		.extract()
+				.body().path("access_token");
+		System.out.println(token);
+	}
+}
