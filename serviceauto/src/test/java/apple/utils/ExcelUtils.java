77b9a41950eb417276caa3501e503ff4dfcaef26
leiyufang <leiyufang@sunlands.com>
2020-10-10 18:00:03 +0800

    ExcelUtils

--- serviceauto/src/test/java/apple/utils/ExcelUtils.java
+++ serviceauto/src/test/java/apple/utils/ExcelUtils.java
@@ -5,29 +5,51 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExcelUtils {
-	@Test
-	public  void getDatas() {
+	public  static Object[][] getDatas() {
 		String excelPath = "src/test/resources/servicecasesv1.xlsx";
+		List<List> lists = null;
+		Object[][] datas = null;
 		try {
 			//获取WORKBOOK对象
 			Workbook workbook = WorkbookFactory.create(new File(excelPath));
 			//获取SHEET对象
 			Sheet sheet = workbook.getSheet("Cases");
 			//获取行
-			Row row = sheet.getRow(2);
-			//获取列
-			Cell cell = row.getCell(6);
-			//将单元格类型设置为STRING类型
-			cell.setCellType(CellType.STRING);
-			String value = cell.getStringCellValue();
-			System.out.println(value);
-			
+			lists = new ArrayList<List>();
+			for (int i = 1;i <= 6; i++ ){
+				//获取行
+				Row row = sheet.getRow(i);
+				List<String> list = new ArrayList<String>();
+				for (int j = 5;j <= 6; j++){
+					Cell cell = row.getCell(j);
+					cell.setCellType(CellType.STRING);
+					String value = cell.getStringCellValue();
+					list.add(value);
+				}
+				lists.add(list);
+			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-//		return null;
+		datas = listToArray(lists);
+		return datas;
+	}
+	
+	public static Object[][] listToArray(List<List> list){
+		//定义一个二维数组接收返回结果
+		Object[][] datas = new Object[list.size()][];
+		for (int i = 0; i<list.size(); i++){
+			List<String> listin = list.get(i);
+			for (int j = 0;j < listin.size(); j++){
+				String value = listin.get(j);
+				datas[i][j] = value;
+			}
+		}
+		return datas;
 	}
 }
