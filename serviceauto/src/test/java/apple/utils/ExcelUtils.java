90c709734dc25a2e251e7987819e1af8fa001175
leiyufang <leiyufang@sunlands.com>
2020-10-21 17:30:40 +0800

    ExcelUtils

--- serviceauto/src/test/java/apple/utils/ExcelUtils.java
+++ serviceauto/src/test/java/apple/utils/ExcelUtils.java
@@ -1,17 +1,15 @@
 package apple.utils;
 
 import org.apache.poi.ss.usermodel.*;
-import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ExcelUtils {
 	public  static Object[][] getDatas() {
 		String excelPath = "src/test/resources/servicecasesv1.xlsx";
-		List<List> lists = null;
+		//定义一个二维数组
+		Object[][] datas = new Object[6][];
 		String value = "";
 		try {
 			//获取WORKBOOK对象
@@ -19,27 +17,23 @@ public class ExcelUtils {
 			//获取SHEET对象
 			Sheet sheet = workbook.getSheet("Cases");
 			//获取行
-			lists = new ArrayList<List>();
 			for (int i = 1;i <= 6; i++ ){
 				//获取行
 				Row row = sheet.getRow(i);
-				List<String> list = new ArrayList<String>();
+				datas[i] = new Object[2];
 				for (int j = 5;j <= 6; j++){
 					//避免拿到的 CELL为NULL
 					Cell cell = row.getCell(j, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
 					cell.setCellType(CellType.STRING);
 					value = cell.getStringCellValue();
 					System.out.println(value);
-					list.add(value);
+					datas[i][j] = value;
 				}
-				lists.add(list);
 			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		Object[][] datas = new Object[lists.size()][];
-		datas = CommonUtils.listToArray(lists);
 		return datas;
 	}
 	
