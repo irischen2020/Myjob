b7b35706dff1cf94f16137ba25734fe01db8e596
leiyufang <leiyufang@sunlands.com>
2020-10-21 17:05:39 +0800

    ExcelUtils

--- serviceauto/src/test/java/apple/utils/CommonUtils.java
+++ serviceauto/src/test/java/apple/utils/CommonUtils.java
@@ -1,4 +1,23 @@
 package apple.utils;
 
+import java.util.List;
+
 public class CommonUtils {
+	
+	//二维LIST转换成二维数组
+	public static Object[][] listToArray(List<List> list){
+		
+		//定义一个二维数组接收返回结果
+		Object[][] datas = new Object[list.size()][];
+		for (int i = 0; i<list.size(); i++){
+			List<String> listin = list.get(i);
+			datas[i] = new Object[listin.size()];
+			for (int j = 0;j < listin.size(); j++){
+				String value = listin.get(j);
+				datas[i][j] = value;
+			}
+		}
+		return datas;
+	}
+	
 }
--- serviceauto/src/test/java/apple/utils/ExcelUtils.java
+++ serviceauto/src/test/java/apple/utils/ExcelUtils.java
@@ -25,15 +25,7 @@ public class ExcelUtils {
 				Row row = sheet.getRow(i);
 				List<String> list = new ArrayList<String>();
 				for (int j = 5;j <= 6; j++){
-//					Cell cell = row.getCell(j);
 					//避免拿到的 CELL为NULL
-//					if (cell == null){
-//						value = "";
-//					}else{
-//						cell.setCellType(CellType.STRING);
-//						value = cell.getStringCellValue();
-//					}
-					//或者这样写,同样可以避免拿到的CELL为NULL
 					Cell cell = row.getCell(j, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
 					cell.setCellType(CellType.STRING);
 					value = cell.getStringCellValue();
@@ -47,50 +39,8 @@ public class ExcelUtils {
 			e.printStackTrace();
 		}
 		Object[][] datas = new Object[lists.size()][];
-		datas = listToArray(lists);
+		datas = CommonUtils.listToArray(lists);
 		return datas;
 	}
 	
-	public static Object[][] listToArray(List<List> list){
-//		String value = null;
-		int wsize = list.size();
-		//定义一个二维数组接收返回结果
-		Object[][] datas = null;
-		for (int i = 0; i<wsize; i++){
-			List<String> listin = list.get(i);
-			int lsize = listin.size();
-			datas = new Object[wsize][lsize];
-			for (int j = 0;j < lsize; j++){
-				String value = listin.get(j);
-				datas[i][j] = value;
-//				System.out.println(value);
-			}
-		}
-		return datas;
-	}
-	
-	public static void main(String[] args) {
-		List<List<String>> list = new ArrayList<List<String>>();
-		
-		List<String> list0 = new ArrayList<String>();
-		List<String> list1 = new ArrayList<String>();
-		list0.add("1234567890");
-		list0.add("");
-		list1.add("13265896589");
-		list1.add("");
-		list.add(list0);
-		list.add(list1);
-		Object[][] datas = new Object[list.size()][];
-//		datas = listToArray(list);
-		for (int i = 0; i<list.size(); i++){
-			List<String> listin = list.get(i);
-			datas[i] = new Object[listin.size()];
-			for (int j = 0;j < listin.size(); j++){
-				String value = listin.get(j);
-				datas[i][j] = value;
-				System.out.println(value);
-			}
-		}
-		
-	}
 }
