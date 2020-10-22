4c9b141dcc612752a41b4e04ad4350e32ca01dfe
leiyufang <leiyufang@sunlands.com>
2020-10-22 14:14:06 +0800

    ExcelUtils

--- serviceauto/src/test/java/apple/pojo/Cases.java
+++ serviceauto/src/test/java/apple/pojo/Cases.java
@@ -1,4 +1,15 @@
 package apple.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cases {
+	private String caseId;
+	private String desc;
+	private String apiId;
+	private String params;
 }
