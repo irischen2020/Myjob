2ad74cdd8df65ac58e38cf4e66aee3662fae4a39
leiyufang <leiyufang@sunlands.com>
2020-10-28 15:46:18 +0800

    ExcelUtils

---
+++ serviceauto/src/test/java/apple/utils/ApiUtils.java
@@ -0,0 +1,16 @@
+package apple.utils;
+
+import apple.pojo.Api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApiUtils {
+	//一次性拿出所有接口信息
+	public static List<Api> apiList = new ArrayList<Api>();
+	static{
+	
+//		apiList = ExcelUtils.load("src/test/resources/servicecasesv3.xlsx","接口信息");
+	
+	}
+}
