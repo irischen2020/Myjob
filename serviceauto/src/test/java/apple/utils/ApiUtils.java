b609d828fb30638c13fea4831bd96a572378e955
leiyufang <leiyufang@sunlands.com>
2020-11-02 17:12:15 +0800

    V4

--- serviceauto/src/test/java/apple/utils/ApiUtils.java
+++ serviceauto/src/test/java/apple/utils/ApiUtils.java
@@ -13,4 +13,40 @@ public class ApiUtils {
 		ExcelUtils.load("src/test/resources/servicecasesv3.xlsx","接口信息",Api.class);
 	
 	}
+	
+	public static String getUrlByApiId(String apiId) {
+		String url ="";
+		//遍历apiList
+		for (int i=0;i<apiList.size();i++){
+			Api api = new Api();
+			api = apiList.get(i);
+			if(apiId.equals(api.getApiId()))
+			{
+				url = api.getUrl();
+			}
+		}
+		return url;
+	}
+	public static String getTypeByApiId(String apiId) {
+		String type ="";
+		//遍历apiList
+		for (int i=0;i<apiList.size();i++){
+			Api api = new Api();
+			api = apiList.get(i);
+			if(apiId.equals(api.getApiId()))
+			{
+				type = api.getType();
+			}
+		}
+		return type;
+	}
+	
+	//测试代码
+//	public static void main(String[] args) {
+//		String apiId = "1";
+//		String url = getUrlByApiId(apiId);
+//		String type = getTypeByApiId(apiId);
+//		System.out.println("url:"+url);
+//		System.out.print("type:" + type);
+//	}
 }
--- serviceauto/src/test/java/com/services/RegisterCase_v6.java
+++ serviceauto/src/test/java/com/services/RegisterCase_v6.java
@@ -1,5 +1,7 @@
 package com.services;
 
+import apple.pojo.Api;
+import apple.utils.ApiUtils;
 import apple.utils.CaseUtils;
 import apple.utils.ExcelUtils;
 import apple.utils.HttpClientUtils;
@@ -14,24 +16,39 @@ import java.util.Map;
 
 public class RegisterCase_v6 {
 	
+//	@Test(dataProvider = "datas")
+//	public void test1(String params){
+//		String url = "http://v.juhe.cn/laohuangli/d";
+//		//将用例中的params中的JSON格式的字符串解析成MAP
+//		Map<String,String> map = new HashMap<String,String>();
+//		map = (Map<String, String>) JSONObject.parse(params);
+//
+//		//POST请求
+////		HttpClientUtils.doPost(url,map);
+//		HttpClientUtils.doService("post",url,map);
+//	}
+	//type,url,map都从EXCEL中获取
 	@Test(dataProvider = "datas")
-	public void test1(String params){
-		String url = "http://v.juhe.cn/laohuangli/d";
+	public void test1(String apiId,String params){
+		String url = ApiUtils.getUrlByApiId(apiId);
+		String type = ApiUtils.getTypeByApiId(apiId);
+		
 		//将用例中的params中的JSON格式的字符串解析成MAP
 		Map<String,String> map = new HashMap<String,String>();
 		map = (Map<String, String>) JSONObject.parse(params);
 		
-		//POST请求
-		HttpClientUtils.doPost(url,map);
+		//请求
+		HttpClientUtils.doService(type,url,map);
 	}
+	
 	@DataProvider
 	public Object[][] datas(){
 
 //		从EXCEL读取数据，封装到二维数组里面
 //		Object[][] datas = ExcelUtils.getDatas();
 		
-		String[] cellNames={"Params"};
-		Object[][] datas = CaseUtils.getCaseDatasByApiId("2",cellNames);
+		String[] cellNames={"ApiId","Params"};
+		Object[][] datas = CaseUtils.getCaseDatasByApiId("1",cellNames);
 		return datas;
 		
 	}
