e6f7113b871f9285f06a90d9db300dc7562ea33b
leiyufang <leiyufang@sunlands.com>
2020-11-04 11:13:56 +0800

    V5

---
+++ serviceauto/src/test/java/com/services/BaseCase.java
@@ -0,0 +1,39 @@
+package com.services;
+
+import apple.pojo.Result;
+import apple.utils.ApiUtils;
+import apple.utils.HttpClientUtils;
+import apple.utils.ResultUtils;
+import com.alibaba.fastjson.JSONObject;
+import org.aspectj.lang.annotation.After;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseCase {
+	//type,url,map都从EXCEL中获取
+	@Test(dataProvider = "datas")
+	public void test(String caseId,String apiId,String params){
+		String url = ApiUtils.getUrlByApiId(apiId);
+		String type = ApiUtils.getTypeByApiId(apiId);
+		
+		//将用例中的params中的JSON格式的字符串解析成MAP
+		Map<String,String> map = new HashMap<String,String>();
+		map = (Map<String, String>) JSONObject.parse(params);
+		
+		//执行请求，将每次请求的数据回写到EXCEL，多次读写EXCEL，造成效率低下。换一种方式，批量回写数据。
+//		String actualResponseData = HttpClientUtils.doService(type,url,map);
+//		ExcelUtils.writeActualResponse("src/test/resources/servicecasesv5.xlsx","用例",caseId,"ActualResponseData",actualResponseData);
+		
+		String actualResponseData = HttpClientUtils.doService(type,url,map);
+		Result result = new Result("用例",caseId,"ActualResponseData",actualResponseData);
+		ResultUtils.resultList.add(result);
+	}
+	@AfterTest()
+	public void afterTest(){
+		//解析resultList中的数据，将数据批量回写到EXCEL里面
+		
+	}
+}
--- serviceauto/src/test/java/com/services/RegisterCase_v5.java
+++ serviceauto/src/test/java/com/services/RegisterCase_v5.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterCase_v5 {
+public class RegisterCase_v5 extends BaseCase{
 	
 	//	@Test(dataProvider = "datas")
 //	public void test1(String params){
@@ -25,24 +25,23 @@ public class RegisterCase_v5 {
 ////		HttpClientUtils.doPost(url,map);
 //		HttpClientUtils.doService("post",url,map);
 //	}
+	//将以下代码抽取出来放在基类中：
+	
 	//type,url,map都从EXCEL中获取
-	@Test(dataProvider = "datas")
-	public void test(String caseId,String apiId,String params){
-		String url = ApiUtils.getUrlByApiId(apiId);
-		String type = ApiUtils.getTypeByApiId(apiId);
-		
-		//将用例中的params中的JSON格式的字符串解析成MAP
-		Map<String,String> map = new HashMap<String,String>();
-		map = (Map<String, String>) JSONObject.parse(params);
-		
-		//执行请求，将每次请求的数据回写到EXCEL，多次读写EXCEL，造成效率低下。换一种方式，批量回写数据。
+//	@Test(dataProvider = "datas")
+//	public void test(String caseId,String apiId,String params){
+//		String url = ApiUtils.getUrlByApiId(apiId);
+//		String type = ApiUtils.getTypeByApiId(apiId);
+//
+//		//将用例中的params中的JSON格式的字符串解析成MAP
+//		Map<String,String> map = new HashMap<String,String>();
+//		map = (Map<String, String>) JSONObject.parse(params);
+//
+//		//执行请求，将数据先暂存到resultList中
 //		String actualResponseData = HttpClientUtils.doService(type,url,map);
-//		ExcelUtils.writeActualResponse("src/test/resources/servicecasesv5.xlsx","用例",caseId,"ActualResponseData",actualResponseData);
-		
-		String actualResponseData = HttpClientUtils.doService(type,url,map);
-		Result result = new Result("用例",caseId,"ActualResponseData",actualResponseData);
-		ResultUtils.resultList.add(result);
-	}
+//		Result result = new Result("用例",caseId,"ActualResponseData",actualResponseData);
+//		ResultUtils.resultList.add(result);
+//	}
 	
 	@DataProvider
 	public Object[][] datas(){
