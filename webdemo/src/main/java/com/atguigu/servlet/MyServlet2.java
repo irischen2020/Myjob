8d9aab3f5e08725e80ffb0ea370b84ff81e0d041
leiyufang <leiyufang@sunlands.com>
2020-11-17 15:52:55 +0800

    webdemo-atguigu

--- webdemo/src/main/java/com/atguigu/servlet/MyServlet2.java
+++ webdemo/src/main/java/com/atguigu/servlet/MyServlet2.java
@@ -8,10 +8,16 @@ import java.io.IOException;
 
 public class MyServlet2 extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String header = request.getHeader("User-Agent");
+		System.out.println("用户浏览器信息："+header);
+		//返回success.html页面
+//		response.sendRedirect("success.html");
 		
+		//或者是转发
+		request.getRequestDispatcher("success.html").forward(request,response);
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		doPost(request,response);
 	}
 }
