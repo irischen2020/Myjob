3ca7e2374bfe3a0d8022eca06a7c53b6891e8ec8
leiyufang <leiyufang@sunlands.com>
2020-11-17 11:43:16 +0800

    webdemo-atguigu

---
+++ webdemo/src/main/java/com/atguigu/servlet/MyServlet.java
@@ -0,0 +1,16 @@
+package com.atguigu.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+		System.out.println("dopost........");
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+		System.out.println("doget.....");
+	}
+}
