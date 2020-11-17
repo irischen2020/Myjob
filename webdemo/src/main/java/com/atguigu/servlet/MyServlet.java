1e5d8fd53ad96ba81d5a52996276841a13e38fd4
leiyufang <leiyufang@sunlands.com>
2020-11-17 14:27:11 +0800

    webdemo-atguigu

--- webdemo/src/main/java/com/atguigu/servlet/MyServlet.java
+++ webdemo/src/main/java/com/atguigu/servlet/MyServlet.java
@@ -1,13 +1,19 @@
 package com.atguigu.servlet;
 
+import javafx.print.Printer;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 		System.out.println("dopost........");
+		//给浏览器响应字符串
+		PrintWriter writer = response.getWriter();
+		writer.write("ssss");
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
