c065dc860bcce3e2f26d560414e4782478193778
leiyufang <leiyufang@sunlands.com>
2020-11-19 11:30:24 +0800

    webdemo-atguigu

---
+++ webdemo/src/main/java/com/atguigu/bean/Person.java
@@ -0,0 +1,8 @@
+package com.atguigu.bean;
+
+public class Person {
+	private String lastName;
+	private Integer age;
+	private String gender;
+	private String email;
+}
---
+++ webdemo/src/test/java/TestPerson.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class TestPerson {
+	@Test
+	public void test(){
+	
+	}
+}
